7bb4eea4ab601651184f80fff875182345109633
rohit-jsfreaky <rohitkashyapmrt@gmail.com>
2025-09-18 12:56:46 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,153 @@
+// index.js
+export const servicesData = [
+  {
+    title: "FullStack Development",
+    description:
+      "Your business deserves a fast, secure, and future-proof digital foundation. I develop custom web apps with clean architecture, optimized databases, and seamless integrations—ensuring reliability at every layer.",
+    items: [
+      {
+        title: "Backend Engineering",
+        description: "(REST/GraphQL APIs, Microservices, Auth Systems)",
+      },
+      {
+        title: "Frontend Excellence",
+        description: "(React, Vue, TypeScript, Interactive UI/UX)",
+      },
+      {
+        title: "Database Design",
+        description: "(SQL/NoSQL Optimization, Scalable Structures)",
+      },
+    ],
+  },
+  {
+    title: "Security & Optimization",
+    description:
+      "Slow or hacked apps destroy trust. I harden security (XSS/SQLI protection, OAuth) and optimize bottlenecks so your app stays fast, safe, and scalable as you grow.",
+    items: [
+      {
+        title: "Code Audits",
+        description: "(Refactoring, Tech Debt Cleanup)",
+      },
+      {
+        title: "Pen Testing",
+        description: "(Vulnerability Assessments)",
+      },
+      {
+        title: "SEO Tech Stack",
+        description: "(SSR, Metadata, Structured Data)",
+      },
+    ],
+  },
+  {
+    title: "Web & Mobile Apps",
+    description:
+      "A clunky interface can sink even the best ideas. I craft responsive, pixel perfect web and mobile apps (React Native) that users love—bridging design and functionality seamlessly.",
+    items: [
+      {
+        title: "Cross-Platform Apps",
+        description: "(Single codebase for iOS/Android/Web)",
+      },
+      {
+        title: "PWAs",
+        description: "(Offline mode, Push Notifications)",
+      },
+      {
+        title: "E-Commerce",
+        description: "(Checkout flows, Payment Gateways, Inventory APIs)",
+      },
+    ],
+  },
+];
+export const projects = [
+  {
+    id: 1,
+    name: "Skill Arena",
+    description:
+      "An online Esport Tournament Management System with team registrations, match scheduling, and real-time score updates.",
+    href: "https://github.com/rohit-jsfreaky/skill-arena-frontend",
+    image: "/assets/projects/skill-arena.png",
+    bgImage: "/assets/backgrounds/blanket.jpg",
+    frameworks: [
+      { id: 1, name: "React" },
+      { id: 2, name: "Node.js" },
+      { id: 3, name: "Express.js" },
+      { id: 4, name: "Typescript" },
+      { id: 5, name: "Shad cn" },
+      { id: 6, name: "PostgreSQL" },
+      { id: 7, name: "Razorpay" },
+    ],
+  },
+  {
+    id: 2,
+    name: "Csv Editor",
+    description:
+      "This project is a robust CSV management platform that combines dynamic table editing, cloud storage with AWS S3, and secure authentication to deliver a seamless and user-friendly experience for managing and editing CSV files.",
+    href: "https://github.com/rohit-jsfreaky/Csv-Editor",
+    image: "/assets/projects/cvs-editor.png",
+    bgImage: "/assets/backgrounds/curtains.jpg",
+    frameworks: [
+      { id: 1, name: "Next.js" },
+      { id: 2, name: "Aws S3" },
+      { id: 3, name: "Shad cn" },
+      { id: 4, name: "Clerk" },
+      { id: 5, name: "Tailwind CSS" },
+      { id: 6, name: "Next Api" },
+    ],
+  },
+  {
+    id: 3,
+    name: "Ai Mock Interview",
+    description:
+      "Built an AI-powered mock interview platform with Next.js, Drizzle ORM, and NeonDB, enhancing user preparation by 80%. Integrated Gemini API for tailored questions and Clerk for secure authentication, ensuring efficiency and scalability.",
+    href: "https://github.com/rohit-jsfreaky/Ai-mock-interview",
+    image: "/assets/projects/ai-interview.png",
+    bgImage: "/assets/backgrounds/map.jpg",
+    frameworks: [
+      { id: 1, name: "Next.js" },
+      { id: 2, name: "Drizzle ORM" },
+      { id: 3, name: "Tailwind CSS" },
+      { id: 4, name: "NeonDB" },
+    ],
+  },
+  {
+    id: 4,
+    name: "Ai Resume Builder",
+    description:
+      "I've developed an AI-powered resume builder using React, Node.js, and MongoDB, featuring AI-driven suggestions via Gemini API and secure authentication with Clerk",
+    href: "https://github.com/rohit-jsfreaky/resume-builder-ai",
+    image: "/assets/projects/resume.png",
+    bgImage: "/assets/backgrounds/poster.jpg",
+    frameworks: [
+      { id: 1, name: "React.js" },
+      { id: 2, name: "Mongo DB" },
+      { id: 3, name: "Tailwind Css" },
+      { id: 4, name: "Node.js" },
+      { id: 5, name: "Express.js" },
+    ],
+  },
+  {
+    id: 5,
+    name: "Job Portal",
+    description:
+      "Developed a job portal using React, Node.js, and MongoDB. Features include secure authentication, job postings, and application management, bridging the gap between students andemployers.",
+    href: "https://github.com/rohit-jsfreaky/job-portal",
+    image: "/assets/projects/job-portal.jpg",
+    bgImage: "/assets/backgrounds/table.jpg",
+    frameworks: [
+      { id: 1, name: "React.js" },
+      { id: 2, name: "Mongo DB" },
+      { id: 3, name: "Tailwind Css" },
+      { id: 4, name: "Node.js" },
+      { id: 5, name: "Express.js" },
+    ],
+  },
+];
+export const socials = [
+  { name: "Instagram", href: "https://www.instagram.com/ali.sanatidev/reels/" },
+  {
+    name: "Youtube",
+    href: "https://www.youtube.com/channel/UCZhtUWTtk3bGJiMPN9T4HWA",
+  },
+  { name: "LinkedIn", href: "https://www.linkedin.com/in/ali-sanati/" },
+  { name: "GitHub", href: "https://github.com/Ali-Sanati" },
+];
